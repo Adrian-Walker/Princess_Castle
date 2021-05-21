3bacd18f27f750f235fd1425771e02510a56bfba
Adrian Walker <awalker3365@gmail.com>
2021-05-21 00:33:26 -0400

    castle1

--- princess.js
+++ princess.js
@@ -1,6 +1,10 @@
 class Character{
-    constructor(name,totalCoins){
+    constructor(name,totalCoins, status){
         this.name = name
         this.totalCoins = totalCoins
+        this.status = status
     }
 }
+
+const Luigi = new Character("Luigi", 100, "Small")
+console.log(Luigi.totalCoins)
