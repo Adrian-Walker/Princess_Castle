8457eedaf0488a8a63d9e41698e1c3e4cc32ef31
Adrian Walker <awalker3365@gmail.com>
2021-07-11 12:55:54 -0400

    almost

--- princess.js
+++ princess.js
@@ -1,10 +1,72 @@
-class Character{
-    constructor(name,totalCoins, status){
+const readlineSync = require('readline-sync')
+
+class Character {
+    constructor(name, totalCoins, status, hasStar = false, gameActive = true) {
         this.name = name
         this.totalCoins = totalCoins
         this.status = status
+        this.hasStar = hasStar
     }
+
+    setName(namePicked) {
+        let players = ['Mario', 'Luigi']
+        namePicked = readlineSync.keyInSelect(players, "Pick Player: ")
+        if (namePicked === '1') {
+            this.name = "Mario"
+        }
+
+        if (namePicked === '2') {
+            this.name = "Luigi"
+        }
+    }
+
+    addCoin() {
+        this.totalCoins + 1
+    }
+
+    gotHit() {
+        if (this.status = 'Powered Up') {
+            this.status = 'Big'
+        } else if (this.status = 'Big') {
+            this.status = 'Small'
+        } else if (this.status = 'Small') {
+            this.status = 'Dead'
+        } else if (this.status = 'Dead') {
+            gameActive = false
+        }
+    }
+
+    gotPowerUp() {
+        if (this.status = 'Small') {
+            this.status = 'Big'
+        } else if (this.status = 'Big') {
+            this.status = 'Powered Up'
+        }
+    }
+
+
+
+    print() {
+        console.log(`Name: ${this.name}`)
+        console.log(`Coins: ${this.totalCoins}`)
+        console.log(`Status: ${this.status}`)
+        console.log(`Stars: ${this.hasStar}`)
+    }
+
 }
 
-const Luigi = new Character("Luigi", 100, "Small")
-console.log(Luigi.totalCoins)
+function randomRange(min, max) {
+    return Math.random() * (2 - 0) + 0;
+}
+
+if (randomRange = 0) {
+    gotHit()
+} else if (randomRange = 1) {
+    gotPowerUp()
+} else if (randomRange = 2) {
+    addCoin()
+}
+
+function setInterval(() => {
+    randomRange()
+}, 5000);
